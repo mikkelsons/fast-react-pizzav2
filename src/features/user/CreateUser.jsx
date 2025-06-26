3d0b8cd6ffe972a7f42d11a03faf55b0bbcaf316
mikkelsons <mikkelson_shawn@yahoo.com>
2025-06-26 11:08:24 -0400

    added redux global state management

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
+import { useDispatch } from "react-redux";
+import { updateName } from "./userSlice";
+import { useNavigate } from "react-router-dom";
 
 function CreateUser() {
   const [username, setUsername] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!username) return;
+
+    dispatch(updateName(username));
+    navigate("/menu");
   }
 
   return (
@@ -16,7 +26,7 @@ function CreateUser() {
 
       <input
         type="text"
-        placeholder="Your full name"
+        placeholder="Your name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         className="input mb-8 w-72"
